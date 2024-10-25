39fbe4fd090b6adc587569722a112d5cf05df9c6
FarmProgrammer <104975790+FarmProgrammer@users.noreply.github.com>
2024-10-25 20:25:23 +0100

    A Simple Graph API Part 2

--- Assets/Graphs/Graph.cs
+++ Assets/Graphs/Graph.cs
@@ -42,4 +42,91 @@ public class Graph
         }
         return null;
     }
+
+    public bool AStar(GameObject startID, GameObject endID)
+    {
+        Node start = FindNode(startID);
+        Node end = FindNode(endID);
+
+        if(start ==null || end == null)
+        {
+            return false;
+        }
+
+        List<Node> open = new List<Node>();
+        List<Node> closed = new List<Node>();
+        float tentativeGScore = 0;
+        bool tentativeIsbetter;
+
+        start.g = 0;
+        start.h = distance(start, end);
+        start.f = start.h;
+
+        open.Add(start);
+        while (open.Count > 0)
+        {
+            int i = lowestF(open);
+            Node thisNode = open[i];
+            if(thisNode.getID() == endID)
+            {
+                //ReconstructPath(start, end);
+                return true;
+            }
+
+            open.RemoveAt(i);
+            closed.Add(thisNode);
+            Node neighbour;
+            foreach(Edge e in thisNode.edgeList)
+            {
+                neighbour = e.endNode;
+                if (closed.IndexOf(neighbour) > -1)
+                    continue;
+
+                tentativeGScore = thisNode.g + distance(thisNode, neighbour);
+
+                if (open.IndexOf(neighbour) == -1)
+                {
+                    open.Add(neighbour);
+                    tentativeIsbetter = true;
+                }
+                else if (tentativeGScore < neighbour.g)
+                {
+                    tentativeIsbetter = true;
+                }
+                else
+                    tentativeIsbetter = false;
+                neighbour.cameFrom = thisNode;
+                neighbour.g = tentativeGScore;
+                neighbour.h = distance(thisNode, end);
+                neighbour.f = neighbour.g + neighbour.h;
+            }
+        }
+        return false;
+    }
+
+    float distance(Node a, Node b)
+    {
+        return (Vector3.SqrMagnitude(a.getID().transform.position - b.getID().transform.position));
+    }
+
+    int lowestF(List<Node> l)
+    {
+        float lowestF = 0;
+        int count = 0;
+        int iteratorCount = 0;
+
+        lowestF = l[0].f;
+
+        for (int i = 1; i < l.Count; i++)
+        {
+            if (l[i].f < lowestF)
+            {
+                lowestF = l[i].f;
+                iteratorCount = count;
+            }
+            count++;
+        }
+
+        return iteratorCount;
+    }
 }
