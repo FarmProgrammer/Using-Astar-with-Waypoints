5cd00da8ecf1b37a3d1de637a14bfd92a36baed4
FarmProgrammer <104975790+FarmProgrammer@users.noreply.github.com>
2024-10-25 20:57:45 +0100

    A Simple Graph API Part 3

--- Assets/WPManager.cs
+++ Assets/WPManager.cs
@@ -2,33 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public struct Link
+{
+    public enum direction
+    {
+        UNI,
+        BI
+    }
+    public GameObject node1;
+    public GameObject node2;
+    public direction dir;
+}
 public class WPManager : MonoBehaviour
 {
-    [System.Serializable]
-    public struct Link
-    {
-        public enum direction
-        {
-            UNI,
-            BI
-        }
-        public GameObject node1;
-        public GameObject node2;
-        public direction dir;
-    }
-
     public GameObject[] waypoints;
     public Link[] links;
+    public Graph graph = new Graph();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (waypoints.Length > 0)
+        {
+            foreach (GameObject wp in waypoints)
+            {
+                graph.AddNode(wp);
+            }
+
+            foreach(Link l in links)
+            {
+                graph.AddEdge(l.node1, l.node2);
+                if(l.dir == Link.direction.BI)
+                {
+                    graph.AddEdge(l.node2, l.node1);
+                }
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnDrawGizmos()
+    {
+        foreach (Link l in links)
+        {
+            Gizmos.DrawLine(l.node1.transform.position, l.node2.transform.position);
+        }
     }
 }
