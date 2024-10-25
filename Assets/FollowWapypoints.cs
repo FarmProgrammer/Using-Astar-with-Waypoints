d9592691393481ad11a629e823729622ef6a478b
FarmProgrammer <104975790+FarmProgrammer@users.noreply.github.com>
2024-10-25 22:03:47 +0100

    Traversing a Path

--- Assets/FollowWapypoints.cs
+++ Assets/FollowWapypoints.cs
@@ -6,7 +6,7 @@ public class FollowWapypoints : MonoBehaviour
 {
     Transform goal;
     float speed = 5;
-    float accuracy = 1;
+    float accuracy = 5;
     float rotspeed = 2;
 
     public GameObject wpManager;
@@ -19,8 +19,9 @@ public class FollowWapypoints : MonoBehaviour
     void Start()
     {
         wps = wpManager.GetComponent<WPManager>().waypoints;
-        g = wpManager.GetComponent<Graph>();
+        g = wpManager.GetComponent<WPManager>().graph;
         currentNode = wps[0];
+        Invoke("GoToRuin", 2);
     }
 
     public void GoToHeli()
@@ -35,9 +36,37 @@ public class FollowWapypoints : MonoBehaviour
         currentWP = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GoToRandom()
     {
-        
+        g.AStar(currentNode, wps[Random.Range(0, wps.Length)]);
+        currentWP = 0;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (g.pathList.Count == 0 || currentWP == g.pathList.Count)
+            return;
+
+        if (Vector3.Distance(transform.position, g.pathList[currentWP].getID().transform.position) < accuracy)
+        {
+            currentNode = g.pathList[currentWP].getID();
+            currentWP++;
+        }
+
+        if (currentWP < g.pathList.Count)
+        {
+            goal = g.pathList[currentWP].getID().transform;
+
+            Vector3 lookAtGoal = new Vector3(goal.position.x, transform.position.y, goal.position.z);
+
+            Vector3 dir = lookAtGoal - transform.position;
+
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                    Quaternion.LookRotation(dir),
+                                                    rotspeed * Time.deltaTime);
+
+            transform.Translate(0, 0, speed * Time.deltaTime);
+        }
     }
 }
