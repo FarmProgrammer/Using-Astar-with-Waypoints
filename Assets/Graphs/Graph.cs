d79add743a974baac2656b39eba5c8bc0f27ff8d
FarmProgrammer <104975790+FarmProgrammer@users.noreply.github.com>
2024-10-25 19:32:07 +0100

    A Simple Graph API Part 1

---
+++ Assets/Graphs/Graph.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph
+{
+    List<Edge> edges = new List<Edge>();
+    List<Node> nodes = new List<Node>();
+    List<Node> pathList = new List<Node>();
+
+    public Graph()
+    {
+
+    }
+
+    public void AddNode(GameObject id)
+    {
+        Node node = new Node(id);
+        nodes.Add(node);
+    }
+
+    public void AddEdge(GameObject fromNode, GameObject toNode)
+    {
+        Node from = FindNode(fromNode);
+        Node to = FindNode(toNode);
+        if(from != null && to != null)
+        {
+            Edge edge = new Edge(from, to);
+            edges.Add(edge);
+            from.edgeList.Add(edge);
+        }
+    }
+
+    Node FindNode(GameObject id)
+    {
+        foreach (Node node in nodes)
+        {
+            if (node.getID() == id)
+            {
+                return node;
+            }
+        }
+        return null;
+    }
+}
